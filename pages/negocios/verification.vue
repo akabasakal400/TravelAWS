883beb55420b171fbc508e5a72c14e130508743a
Aykut Kabasakal <akabasakal400@gmail.com>
2021-09-22 11:22:23 -0600

    Verificacion de negocios

--- pages/negocios/verification.vue
+++ pages/negocios/verification.vue
@@ -5,7 +5,7 @@
                 <v-parallax :src="imageLink.sub_main" height="800">
                     <v-layout column align-center justify-center class="white--text">
                         <h1 class="white--text mb-2 display-1 text-xs-center" style="font-weight: 900; text-shadow: 3px 2px #000000">Bienvenido a la familia Antigua Travel, Comencemos...</h1>
-                        <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">
+                        <v-btn class="blue lighten-2 mt-5" dark large href="/">
                         Inicio
                         </v-btn>
                     </v-layout>
